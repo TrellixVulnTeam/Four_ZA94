60a548a2a820cf7e2aac5ec1c1062b0bb80a5fa1
TechGroup2020 <65551463+TechGroup2020@users.noreply.github.com>
2022-05-04 21:11:53 +0530

    Update front.sh

--- front.sh
+++ front.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 docker stop frontcont17
 docker rm frontcont17
-docker run -i --name frontcont17 --network=gomatha --ip 192.168.1.4  -p 5050:5050  frontimg17 /bin/bash
-docker run -it --name frontcont17 frontimg17 /bin/bash
+docker run -it --name frontcont17 --network=gomatha --ip 192.168.1.4  -p 5050:5050  frontimg17 /bin/bash
 docker start frontcont17
 docker exec frontcont17  ./startup.sh
