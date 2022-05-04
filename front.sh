a28cd94e1f5bca8f2bebecdd14584481729fde95
TechGroup2020 <65551463+TechGroup2020@users.noreply.github.com>
2022-05-04 22:07:59 +0530

    Delete front.sh

--- front.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker stop frontcont17
-docker rm frontcont17
-docker run -it --name frontcont17 --network=gomatha --ip 192.168.1.4  -p 5050:5050  frontimg17 /bin/bash
-docker start frontcont17
-docker exec frontcont17  ./startup.sh
